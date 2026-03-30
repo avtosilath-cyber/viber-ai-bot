3d3255fae550e00950cb64def843ab48d18d5ca6
avtosilath-cyber <avtosilath@gmail.com>
2026-03-30 13:57:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-import os
-from fastapi import FastAPI, Request
-import requests
-
-app = FastAPI()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-URL = f"https://api.telegram.org/bot{TOKEN}"
-
-@app.get("/")
-def home():
-    return {"status": "ok"}
-
 @app.post("/webhook")
 async def webhook(req: Request):
     data = await req.json()
 
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
+    message = data.get("message")
+    if not message:
+        return {"ok": True}
 
-        requests.post(f"{URL}/sendMessage", json={
-            "chat_id": chat_id,
-            "text": f"Ты написал: {text}"
-        })
+    chat_id = message["chat"]["id"]
+    text = message.get("text", "")
+
+    requests.post(f"{URL}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": f"Ты написал: {text}"
+    })
 
     return {"ok": True}
